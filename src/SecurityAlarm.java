86d6d7744a7e73225b2f3d42d2ae073a21ded9a8
iamcharlie17 <riyadhosen40@gmail.com>
2025-02-24 09:02:09 +0600

    imp: security alarm class

--- src/SecurityAlarm.java
+++ src/SecurityAlarm.java
@@ -1,2 +1,25 @@
-public class SecurityAlarm {
+class SecurityAlarm extends NotifiableDevice implements SecurityDevice {
+    private String password;
+
+    public SecurityAlarm(String location, String password) {
+        super("Security Alarm", location);
+        this.password = password;
+    }
+
+    @Override
+    public void verifyPassword(String password) {
+        if (this.password.equals(password)) {
+            System.out.println("Access granted.");
+        } else {
+            System.out.println("Access denied.");
+            if (notificationsEnabled) {
+                System.out.println("User notified about failed password attempt.");
+            }
+        }
+    }
+
+    @Override
+    public void adjustSettings() {
+        System.out.println("Adjusting security alarm settings...");
+    }
 }
