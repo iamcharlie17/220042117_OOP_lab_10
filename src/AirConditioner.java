c9fb14df00d75d649609791264a73bc093274068
iamcharlie17 <riyadhosen40@gmail.com>
2025-02-24 09:00:10 +0600

    imp: air conditioner class

--- src/AirConditioner.java
+++ src/AirConditioner.java
@@ -1,2 +1,18 @@
-public class AirConditioner {
+class AirConditioner extends SmartDevice implements PoweredDevice{
+    private String fanSpeed;
+    private int temparature;
+
+    public AirConditioner(String location) {
+        super("Air Conditioner", location);
+    }
+
+    @Override
+    public void adjustSettings() {
+        System.out.println("Adjusting AC settings...");
+    }
+
+    @Override
+    public void adjustPowerSettings() {
+        System.out.println("Setting fan speed and temperature.");
+    }
 }
