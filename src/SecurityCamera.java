cbe5d01f3644f9b2fa4978bdcbb0ddd90f633965
iamcharlie17 <riyadhosen40@gmail.com>
2025-02-24 09:05:51 +0600

    impl: security camera class

--- src/SecurityCamera.java
+++ src/SecurityCamera.java
@@ -1,2 +1,17 @@
-public class SecurityCamera {
+class SecurityCamera extends NotifiableDevice {
+    private boolean autoSave;
+
+    public SecurityCamera(String location) {
+        super("Security Camera",location);
+    }
+
+    public void toggleAutoSave() {
+        autoSave = !autoSave;
+        System.out.println("Auto-save turned " + (autoSave ? "ON" : "OFF"));
+    }
+
+    @Override
+    void adjustSettings() {
+        System.out.println("Adjusting security camera settings");
+    }
 }
