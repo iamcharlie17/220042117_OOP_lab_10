794d2d04e8f29e18754ac9e377ece8c9d9deb60f
iamcharlie17 <riyadhosen40@gmail.com>
2025-02-24 09:12:05 +0600

    impl: smoke detector class

--- src/SmokeDetector.java
+++ src/SmokeDetector.java
@@ -1,2 +1,27 @@
-public class SmokeDetector {
+class SmokeDetector extends NotifiableDevice implements SensoryDevice{
+    private boolean sprinkleActive;
+
+    public SmokeDetector(String location) {
+        super("Smoke Detector", location);
+    }
+
+    public void activateSprinkle(){
+        sprinkleActive = true;
+        System.out.println("Sprinkle activated.");
+    }
+
+    public void deactivateSprinkle(){
+        sprinkleActive = false;
+        System.out.println("Sprinkle deactivated.");
+    }
+
+    @Override
+    public void measure() {
+        System.out.println("Measuring smoke levels...");
+    }
+
+    @Override
+    public void adjustSettings() {
+        System.out.println("Adjusting smoke detector settings...");
+    }
 }
