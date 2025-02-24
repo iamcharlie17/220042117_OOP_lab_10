f5dfa94997dabf0fc21454516b4440ceb95532ed
iamcharlie17 <riyadhosen40@gmail.com>
2025-02-24 08:56:48 +0600

    imp: light class

--- src/Light.java
+++ src/Light.java
@@ -1,2 +1,19 @@
-public class Light {
+class Light extends SmartDevice implements PoweredDevice {
+    private int intensity;
+    private String color;
+
+    public Light(String location) {
+        super("Light", location);
+    }
+
+
+    @Override
+    public void adjustPowerSettings() {
+        System.out.println("Adjusting light intensity and color.");
+    }
+
+    @Override
+    void adjustSettings() {
+        System.out.println("Adjusting light settings");
+    }
 }
